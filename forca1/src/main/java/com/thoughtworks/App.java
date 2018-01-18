4aeb35d6179e6ab70fa16d029cd04231f2eea4a4
Biamichelle <33293201+bbiamichelle@users.noreply.github.com>
2018-01-18 18:53:05 -0200

    Update App.java

--- forca1/src/main/java/com/thoughtworks/App.java
+++ forca1/src/main/java/com/thoughtworks/App.java
@@ -7,15 +7,18 @@ public class App
     {
         System.out.println(" Você tem que advinhar a palavra que eu estou pensando");
         System.out.println(" Digite uma palavra: ");
-
-        Scanner scan = new Scanner(System.in);
-        String nome = scan.nextLine().trim();
-
-        if (nome .equals("Casa")){
-            System.out.println("Acertou");
-
-        } else {
-            System.out.println("Você não acertou, reflita um pouco e tente novamente mais tarde!");
+        String nome = "";
+        
+            while ( !nome.equals("Casa")){
+            Scanner scan = new Scanner(System.in);
+            nome = scan.nextLine().trim();
+            
+            if (nome .equals("Casa")){
+                System.out.println("Acertou");
+            } else {
+                System.out.println("Você não acertou, tente de novo");
+            }
+            
         }
     }
 }
